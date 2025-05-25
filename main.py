c2c8933eb3d63557bf9f10c206178b93feec3c53
yangdexiang <1059364927@qq.com>
2025-05-25 15:12:04 +0800

    第一次提交

---
+++ main.py
@@ -0,0 +1,20 @@
+
+def add(a, b):
+    """
+    返回两个数的和
+
+    参数:
+    a (int 或 float): 第一个加数
+    b (int 或 float): 第二个加数
+
+    返回:
+    int 或 float: 两个数的和
+    """
+    return a + b
+
+# 测试函数
+result = add(5, 3)
+print("5 + 3 =", result)  # 输出: 5 + 3 = 8
+
+result = add(2.5, 3.7)
+print("2.5 + 3.7 =", result)  # 输出: 2.5 + 3.7 = 6.2
